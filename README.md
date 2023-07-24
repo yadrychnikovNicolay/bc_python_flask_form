0bccaf210086da59a3b3b724aa165d37c43b4edc
yadrychnikovNicolay <yadrychnikov.n@gmail.com>
2023-07-24 09:44:22 +0200

    init + form

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # bc_python_flask_form
 BeCode Python - Flask Form Module
+
+## Rules
+
+### Performance criteria
+
+- If there is an error in the user input, return the form with valid fields still filled.
+- Show the field errors near the field in question.
+- The form should sanitize and have server side validation.
+- If the sanitazation and validation are ok, a "Thank you for your message" page should show with a run-down of all the information the user input.
+- Implement antispam and honeypots
+- Required fields:
+    - Full name
+    - Email
+    - Country (list)
+    - Message
+    - Gender (radio box)
+    - Subject (Repair, Order, Other)
+- All fields are requiered except "Subject", which will default to "Other" value if not entered
+
+
+### Goals
+
+- Presentation: architechture client/server
+- Sanitazation: neutralize all malicious code (script tags)
+- Validation: required fields + valid email
+- Confirmation: feedback when message sent
+- NO JS OR CSS
+
+
+### End goals
+
+At the end of the project, the following skills should be acquired:
+
+- Explain the difference betwenn a POST and GET request
+- Avoid XSS attacks
+- Avoid SSTI attacks
+- Use a micro framework
+- Deploy an app
+
+
+## Running the project
+
+```flask run```
+
+Navigate to the url the terminal gives you (localhost:5000), you can see an example message already displayed.
+
+Navigate to "Create" and create a message
---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5767c4f5ed4d7a03e49a402bfb91cd5cbff8a65807cb7df1'
+
+messages = [{'fullname': 'Name Example',
+             'email': 'email@example.com',
+             'country': 'Example Country',
+             'message': 'Message message message',
+             'subject': 'Other',
+             'gender': 'Male'}]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', messages=messages)
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        email = request.form['email']
+        country = request.form['country']
+        message = request.form['message']
+        subject = request.form['subject']
+        gender = request.form['gender']
+
+        if not fullname:
+            flash('Full name is required!')
+        elif not email:
+            flash('Email is required!')
+        elif not country:
+            flash('Country is required!')
+        elif not message:
+            flash('Message is required!')
+        elif not gender:
+            flash('Gender is required!')
+        else:
+            messages.append({
+                'fullname': fullname,
+                'email': email,
+                'country': country,
+                'message': message,
+                'subject': subject,
+                'gender': gender
+            })
+            return redirect(url_for('index'))
+    return render_template('create.html')
